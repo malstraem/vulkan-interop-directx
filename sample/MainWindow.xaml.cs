b41b7f9656330a53046282ce772c200961392fc9
Malstraem <59914970+Malstraem@users.noreply.github.com>
2023-03-02 14:53:39 +0300

    initial project structure with winui3 sample app

---
+++ sample/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace SwapchainApp.WinUI3
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
